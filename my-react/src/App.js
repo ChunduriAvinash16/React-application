661114ba5deb3519921cb80460d6747f8136278e
Avinash <chunduriaviansh6@gmail.com>
2020-10-22 22:03:45 +0530

    Hello

---
+++ my-react/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./assets/css/style.css";
+/*class App extends Component{
+    constructor(props){
+        super(props); //default props
+        this.state={title:"Hello React 2"};
+
+    }
+    render(){
+        return(
+            <h2>{this.state.title}</h2>
+        )
+    }
+}
+
+export default App;*/
+
+
+
+function App() {
+    return (
+        <div>
+            <div className="Hello">
+                <h2 >Hello react</h2>
+            </div>
+        </div>
+    )
+}
+
+export default App
--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,10 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-function App(){
-    return(
-        <h2>Hello</h2>
-    );
-}
+import App from "./App";
 
 ReactDOM.render(<App/>,document.getElementById("root"));
+//render(newelment , destinationelment)
