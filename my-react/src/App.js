5bbbb8d88e02fc6fde1861daf63594d84c373a29
Avinash <chunduriaviansh6@gmail.com>
2020-10-31 23:34:56 +0530

    Login Form

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -3,17 +3,22 @@ import "./assets/css/style.css";
 import Images from "./components/Images";
 import {BrowserRouter as Router,Route, Switch } from "react-router-dom";
 import route from './utils/route';
+import Header from './components/Header';
 function App() {
   //  const [title, seTtitle] = useState("Hello react 2");
     return (
         <Router>
-            {route.map((r)=>(
+           <Header/>            
+            <Switch>
+            {route.map((r,index)=>(
                 <Route
+                 key={index}
                  path={r.path}
                  exact={r.exact}
                  component={r.component}
                 />
             ))}
+            </Switch>
         </Router>
 
     )
--- my-react/src/pages/Login.js
+++ my-react/src/pages/Login.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 export default function Login() {
+    function handleForm(e) {
+        e.preventDefault();
+        console.log("submitted");
+    }
     return (
-        <div className="flex h-screen">
-            <h3 className="m-auto text-3xl">Login</h3>
+        <div className="flex h-screen bg-gray-200">
+            <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-r from-indigo-900 to-blue-400">
+                <h1 className="w-full text-4xl tracking-widest text-center my-6">Login</h1>
+                <form className="m-5 w-5/6" onSubmit={handleForm}>
+                    <div className="w-full my-6">
+                        <input type="email" className="p-2 rounded shadow w-full" placeholder="Email or UserName"/>
+                    </div>
+                    <div className="w-full my-6">
+                        <input type="password" className="p-2 rounded shadow w-full" placeholder="Password"/>
+                    </div>
+                    <div className="w-full my-10">
+                        <button type="submit" className="p-2 rounded shadow border-blue-100 w-full bg-gradient-to-r from-gray-900 to-blue-300 text-white">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
